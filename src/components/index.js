9b62a9ca03d5a23523af622450917bd99356c73f
Eric Greene <eric@t4d.io>
2018-09-25 07:05:35 -0700

    minor tweak

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,3 @@ export { WidgetTable } from './WidgetTable';
 export { WidgetForm } from './WidgetForm';
 export { Notification } from './Notification';
 export { InfoNotification } from './InfoNotification';
-export { SubscriptionInfoNotification } from './SubscriptionInfoNotification';
